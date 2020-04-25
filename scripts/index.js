1d4ff07094f23d6167f78fb95ae1749002c2575a
Jessica Hong <jwh296@cornell.edu>
2020-04-24 19:39:21 -0500

    trying projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,30 @@
-function generateButton(parent, href, title) {
-    parent.append("div")
-        .style("display", "flex")
-        .style("align-items", "center")
-        .append("a")
-        .style("text-decoration", "none")
-        .attr("href", href)
-        .append("div")
-        .attr("class", "button")
-        .text(title)
+function generateProjects(data, section) {
+    data.forEach(d => {
+        mainrow = section.append("div")
+            .attr("class", "mainrow")
+        mainrow.append("div")
+            .attr("class", "columnleft")
+            .append("img")
+            .attr("draggable", false)
+            .attr("ondragstart", "return false;")
+            .attr("src", d.image)
+            .style("width", "100%")
+        columnright = mainrow.append("div")
+            .attr("class", "columnright")
+        columnright.append("div")
+            .style("display", "flex")
+            .style("align-items", "center")
+            .style("min-height", 36)
+            .append("h3")
+            .text(d.title)
+        columnright.append("h4").text(d.role + " // " + d.time)
+        d.description.forEach(desc => { columnright.append("h6").text(desc) });
+        d.links.forEach(l => {
+           columnright.append("div")
+               .append("a")
+               .attr("href", href)
+               .attr("class", "button")
+               .text(" → " + title)
+        })
+    });
 }
